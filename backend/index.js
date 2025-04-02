6cf1538579a404ab760254b929e07e25233bb259
RahatReza <rahatreza3199@gmail.com>
2025-04-03 02:37:11 +0530

    check error

--- backend/index.js
+++ backend/index.js
@@ -30,6 +30,8 @@ const allowedOrigins = process.env.NODE_ENV === 'production'
     ? [process.env.FRONTEND_URL] 
     : ['http://localhost:5173', 'http://localhost:5174']
 
+console.log(allowedOrigins)
+
 app.use(cors({
     origin: allowedOrigins,
     credentials: true,
