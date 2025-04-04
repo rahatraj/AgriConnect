49c6e8c09515537aa40529883d6e1a4f149a5ea2
RahatReza <rahatreza3199@gmail.com>
2025-04-05 00:42:52 +0530

    token updation

--- backend/app/middlewares/authenticate.js
+++ backend/app/middlewares/authenticate.js
@@ -4,7 +4,7 @@ import CustomError from '../utils/CustomError.js';
 const AuthenticateUser = (req,res,next) => {
     const token = req.cookies.token;
     if(!token){
-        return next(new CustomError("Unathenticated user", 403))
+        return next(new CustomError("Unathenticated user", 401))
     }
 
     try {
