e892288e774d6e3603347f7107816c9a4474122f
RahatReza <rahatreza3199@gmail.com>
2025-04-05 20:07:27 +0530

    debug import issues

--- backend/index.js
+++ backend/index.js
@@ -25,11 +25,8 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser())
 
-// CORS configuration
-const allowedOrigins = process.env.FRONTEND_URL
-
 app.use(cors({
-    origin: allowedOrigins,
+    origin: "https://agriconnect-bid.netlify.app",
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE','PATCH', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
